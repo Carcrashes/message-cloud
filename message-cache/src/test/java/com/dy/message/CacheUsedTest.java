4157b784c6844b1ccdae8029cd4c49e5cf53e24d
dingyu <dingyu4849@qq.com>
2023-04-06 08:09:31 +0800

    first commit

---
+++ message-cache/src/test/java/com/dy/message/CacheUsedTest.java
@@ -0,0 +1,29 @@
+package com.dy.message;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CacheUsedTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+     @Test
+    public void testCacheHash(){
+         Map<String, Object> map=new HashMap<>();
+         map.put("phone","17680154849");
+         map.put("birthday", LocalDate.now());
+         redisTemplate.opsForHash().putAll("test:hash:1",map);
+     }
+}
